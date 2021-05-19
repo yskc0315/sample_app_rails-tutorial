3f5f004d67872e4de1b2138e241d421de58e1b4b
yskc0315 <ihcikihsoy1503@gmail.com>
2021-05-19 13:28:44 +0000

    basic-login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authentificate(params[:session][:password])
+      
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
+end
